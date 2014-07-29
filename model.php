f88b94f05f8877f016dcf328b969d31130975f6a
Lukas Greblikas <lukas.greblikas@gmail.com>
2014-07-29 16:08:06 +0100

    Changed data keys

--- model.php
+++ model.php
@@ -17,14 +17,14 @@ class PillsModel {
 		if($result = $this->mysqli->query("SELECT * FROM Cells WHERE user_id='$userId'")){
 			$data = Array();
 			for (; $row = $result->fetch_array();){
-				$data[$row["cell_index"]]["deadline"] = $row["deadline"];
+				$data[$row["cell_index"]]["time"] = $row["deadline"];
 				$data[$row["cell_index"]]["importance"] = $row["importance"];
-				$data[$row["cell_index"]]["names"] = Array();
+				$data[$row["cell_index"]]["pills"] = Array();
 			}
 				
 			if($result = $this->mysqli->query("SELECT * FROM Names_in_cells WHERE user_id='$userId'")) {
 				for (; $row = $result->fetch_array();)
-					array_push($data[$row["cell_index"]]["names"], $row["name"]);
+					array_push($data[$row["cell_index"]]["pills"], $row["name"]);
 			}
 			return $data;
 		} else 
